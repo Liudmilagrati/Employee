53d9da39705544abc736612b998044f58b69eb41
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-03-03 17:02:28 +0000

    code to search and display
    the name of each of the employees in the projectGroup array,
        who have an employee number above the value stored in a variable called m.
    As example used m = 1

--- src/employee/EmployeeTest.java
+++ src/employee/EmployeeTest.java
@@ -27,6 +27,16 @@ package employee;
     
     //Print out the value of variable nextEmpNum to the terminal window
     System.out.println("The value of the Next Employee Number is: " + Employee.getNextEmpNum());
+    
+    /* Write the code to search and display the name of each of the employees in the projectGroup array, 
+    who have an employee number above the value stored in a variable called m.
+    */
+    int m = 1; // Example value for variable 'm'
+        for (Employee employee : projectGroup) {
+            if (employee.getEmpNum() > m) {
+                System.out.println("Employee Name: " + employee.getName());
+            }
+        }
         
         
         
