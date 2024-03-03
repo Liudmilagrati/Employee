1c0fbb185d677627c16acdc5f9dc7645b8d79f11
Liudmilagrati <120527185+Liudmilagrati@users.noreply.github.com>
2024-03-03 16:23:24 +0000

    Initial commit

---
+++ src/employee/Employee.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package employee;
+
+/** Programmer: Liudmila Grati 
+ * CA2: 
+ * 
+ */
+public class Employee {
+
+    /** Part 1:	Write the complete class, Employee, which has the following fields and methods:
+    Instance fields:
+    name, of type String
+    email, of type String
+    empNum, of type int
+     */
+    
+     // Instance fields
+    private String name;
+    private String email;
+    private int empNum;
+  
+    }
+    
+
